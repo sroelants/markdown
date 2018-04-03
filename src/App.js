9d609103e07cb523f90293f47e408f23ebf08735
Sam Roelants <roelants.sam@gmail.com>
2018-04-03 18:20:34 +0200

    Removed header, changed colors

--- src/App.js
+++ src/App.js
@@ -151,13 +151,13 @@ class App extends Component {
     this.setState({ format: f === "markdown" ? "markdown" : "html" });
   }
 
+  // <header className="App-header">
+  //   <img src={logo} className="App-logo" alt="logo" />
+  //   <h1 className="App-title">Welcome to React</h1>
+  // </header>
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <main>
           <Input handler={e => this.inputHandler(e)} />
           <Output
