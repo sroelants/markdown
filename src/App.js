e8249c4648fa768ae477237ca403368413c1c66e
Sam Roelants <roelants.sam@gmail.com>
2018-04-03 12:16:40 +0200

    Working parsing and toggles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+var myMarked = require("marked");
 
 //*****************************************************************************
 //
@@ -12,7 +13,7 @@ class InputHeader extends Component {
   render() {
     return (
       <div className="box__header">
-        <p>Input header!</p>
+        <p className="box__label">Input</p>
       </div>
     );
   }
@@ -25,13 +26,12 @@ class InputField extends Component {
 
   render() {
     return (
-      <div className="box__field">
-        <textarea
-          type="text"
-          placeholder="Input"
-          onKeyUp={e => this.handleKeyUp(e)}
-        />
-      </div>
+      <textarea
+        type="text"
+        placeholder="Markdown goes here."
+        onKeyUp={e => this.handleKeyUp(e)}
+        className="box__field"
+      />
     );
   }
 }
@@ -57,23 +57,52 @@ class OutputHeader extends Component {
   render() {
     return (
       <div className="box__header">
-        <p>Output header!</p>
+        <p className="box__label">Output</p>
+        <div className="switches">
+          <p
+            className={
+              "switches__switch" +
+              (this.props.format === "html"
+                ? " switches__switch--active"
+                : " switches__switch--inactive")
+            }
+            onClick={() => this.props.switchHandler("html")}
+          >
+            HTML
+          </p>
+          <p
+            className={
+              "switches__switch" +
+              (this.props.format === "markdown"
+                ? " switches__switch--active"
+                : " switches__switch--inactive")
+            }
+            onClick={() => this.props.switchHandler("markdown")}
+          >
+            Markdown
+          </p>
+        </div>
       </div>
     );
   }
 }
 
 class OutputField extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { content: "Output stub" };
-  }
   render() {
-    return (
-      <div className="box__field">
-        <pre>{this.props.content}</pre>
-      </div>
-    );
+    if (this.props.format === "markdown") {
+      return (
+        <div
+          className="box__field"
+          dangerouslySetInnerHTML={{ __html: this.props.output }}
+        />
+      );
+    } else {
+      return (
+        <div className="box__field">
+          <pre>{this.props.output}</pre>
+        </div>
+      );
+    }
   }
 }
 
@@ -81,8 +110,16 @@ class Output extends Component {
   render() {
     return (
       <div className="box">
-        <OutputHeader className="box box_header" />
-        <OutputField className="box box__field" content={this.props.content} />
+        <OutputHeader
+          className="box_header"
+          switchHandler={f => this.props.switchHandler(f)}
+          format={this.props.output.format}
+        />
+        <OutputField
+          className="box__field"
+          format={this.props.output.format}
+          output={this.props.output.output}
+        />
       </div>
     );
   }
@@ -97,12 +134,21 @@ class Output extends Component {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { output: "Output stub" };
+    this.state = {
+      input: "",
+      output: "",
+      format: "html"
+    };
   }
 
-  inputHandler(input) {
-    console.log(input);
-    this.setState({ output: input });
+  // Parse the input using the marked.js library.
+  inputHandler(i) {
+    this.setState({ input: i, output: myMarked(i) });
+  }
+
+  // Handler for setting the output format.
+  outputSwitchHandler(f) {
+    this.setState({ format: f === "markdown" ? "markdown" : "html" });
   }
 
   render() {
@@ -112,8 +158,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Input handler={e => this.inputHandler(e)} />
-        <Output content={this.state.output} />
+        <main>
+          <Input handler={e => this.inputHandler(e)} />
+          <Output
+            output={this.state}
+            switchHandler={f => this.outputSwitchHandler(f)}
+          />
+        </main>
       </div>
     );
   }
